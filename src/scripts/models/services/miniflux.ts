50d9ede85c8da09a495d8c5cab6c62953c438366
Neko Aria <990879119@qq.com>
2023-11-09 09:49:16 +0800

    fix(services/miniflux): resolve issue with Miniflux support

--- src/scripts/models/services/miniflux.ts
+++ src/scripts/models/services/miniflux.ts
@@ -5,7 +5,7 @@ import { ServiceHooks } from "../service"
 import { ServiceConfigs, SyncService } from "../../../schema-types"
 import { createSourceGroup } from "../group"
 import { RSSSource } from "../source"
-import { domParser, htmlDecode } from "../../utils"
+import { htmlDecode } from "../../utils"
 import { RSSItem } from "../item"
 import { SourceRule } from "../rule"
 
@@ -56,7 +56,7 @@ async function fetchAPI(
     configs: MinifluxConfigs,
     endpoint: string = "",
     method: string = "GET",
-    body: string = null
+    body: string = null,
 ): Promise<Response> {
     try {
         const headers = new Headers()
@@ -67,29 +67,79 @@ async function fetchAPI(
             : headers.append("Authorization", `Basic ${configs.authKey}`)
 
         let baseUrl = configs.endpoint
-        if (!baseUrl.endsWith("/")) baseUrl = baseUrl + "/"
-        if (!baseUrl.endsWith("/v1/")) baseUrl = baseUrl + "v1/"
-        const response = await fetch(baseUrl + endpoint, {
+        if (!baseUrl.endsWith("/")) {
+            baseUrl += "/"
+        }
+        if (!baseUrl.endsWith("/v1/")) {
+            baseUrl += "v1/"
+        }
+        return await fetch(baseUrl + endpoint, {
             method: method,
             body: body,
             headers: headers,
         })
-
-        return response
     } catch (error) {
         console.log(error)
         throw APIError()
     }
 }
 
+async function fetchEntries(
+    configs: MinifluxConfigs,
+    queryParams: URLSearchParams,
+    fetchNew: boolean = false,
+    fetchLimit: number = 125,
+): Promise<Entry[]> {
+    const items: Entry[] = []
+    let entriesResponse: Entries | null = null
+
+    // parameters
+    configs.lastId = configs.lastId ?? 0
+    let continueId: number | undefined
+    queryParams.set("limit", String(fetchLimit))
+
+    do {
+        try {
+            if (continueId) {
+                queryParams.set("before_entry_id", String(continueId))
+            } else if (fetchNew) {
+                queryParams.set("after_entry_id", String(configs.lastId))
+            }
+
+            const queryString = queryParams.toString()
+            entriesResponse = await fetchAPI(
+                configs,
+                `entries?${queryString}`,
+            ).then(response => response.json())
+            if (entriesResponse.entries.length === 0) {
+                break
+            }
+
+            items.push(...entriesResponse.entries)
+            if (fetchNew) {
+                continueId = items[items.length - 1].id
+            } else {
+                continueId =
+                    entriesResponse.entries[entriesResponse.entries.length - 1]
+                        .id
+            }
+        } catch {
+            break
+        }
+    } while (
+        entriesResponse &&
+        entriesResponse.total >= fetchLimit &&
+        items.length < configs.fetchLimit
+    )
+
+    return items
+}
+
 export const minifluxServiceHooks: ServiceHooks = {
     // poll service info endpoint to verify auth
     authenticate: async (configs: MinifluxConfigs) => {
         const response = await fetchAPI(configs, "me")
-
-        if (await response.json().then(json => json.error_message)) return false
-
-        return true
+        return !(await response.json().then(json => json.error_message))
     },
 
     // collect sources from service, along with associated groups/categories
@@ -100,7 +150,7 @@ export const minifluxServiceHooks: ServiceHooks = {
         if (configs.importGroups) {
             const groups: Category[] = await fetchAPI(
                 configs,
-                "categories"
+                "categories",
             ).then(response => response.json())
             groups.forEach(group => dispatch(createSourceGroup(group.title)))
         }
@@ -109,13 +159,15 @@ export const minifluxServiceHooks: ServiceHooks = {
         const feedResponse = await fetchAPI(configs, "feeds")
         const feeds = await feedResponse.json()
 
-        if (feeds === undefined) throw APIError()
+        if (feeds === undefined) {
+            throw APIError()
+        }
 
         // go through feeds, create typed source while also mapping by group
-        let sources: RSSSource[] = new Array<RSSSource>()
-        let groupsMap: Map<string, string> = new Map<string, string>()
-        for (let feed of feeds) {
-            let source = new RSSSource(feed.feed_url, feed.title)
+        const sources: RSSSource[] = new Array<RSSSource>()
+        const groupsMap: Map<string, string> = new Map<string, string>()
+        for (const feed of feeds) {
+            const source = new RSSSource(feed.feed_url, feed.title)
             // associate service christened id to match in other request
             source.serviceRef = feed.id.toString()
             sources.push(source)
@@ -133,57 +185,32 @@ export const minifluxServiceHooks: ServiceHooks = {
     fetchItems: () => async (_, getState) => {
         const state = getState()
         const configs = state.service as MinifluxConfigs
-        const items: Entry[] = new Array()
-        let entriesResponse: Entries
 
-        // parameters
-        configs.lastId = configs.lastId ?? 0
-        // intermediate
-        const quantity = 125
-        let continueId: number
-
-        do {
-            try {
-                if (continueId) {
-                    entriesResponse = await fetchAPI(
-                        configs,
-                        `entries?order=id&direction=desc&after_entry_id=${configs.lastId}&before_entry_id=${continueId}&limit=${quantity}`
-                    ).then(response => response.json())
-                } else {
-                    entriesResponse = await fetchAPI(
-                        configs,
-                        `entries?order=id&direction=desc&after_entry_id=${configs.lastId}&limit=${quantity}`
-                    ).then(response => response.json())
-                }
-
-                items.push(...entriesResponse.entries)
-                continueId = items[items.length - 1].id
-            } catch {
-                break
-            }
-        } while (
-            entriesResponse.entries &&
-            entriesResponse.total >= quantity &&
-            items.length < configs.fetchLimit
-        )
+        const queryParams = new URLSearchParams({
+            order: "id",
+            direction: "desc",
+        })
+        const items: Entry[] = await fetchEntries(configs, queryParams, true)
 
         // break/return nothing if no new items acquired
-        if (items.length === 0) return [[], configs]
+        if (items.length === 0) {
+            return [[], configs]
+        }
         configs.lastId = items[0].id
 
         // get sources that possess ref/id given by service, associate new items
         const sourceMap = new Map<string, RSSSource>()
-        for (let source of Object.values(state.sources)) {
+        Object.values(state.sources).forEach(source => {
             if (source.serviceRef) {
                 sourceMap.set(source.serviceRef, source)
             }
-        }
+        })
 
         // map item objects to rssitem type while appling rules (if exist)
         const parsedItems = items.map(item => {
             const source = sourceMap.get(item.feed.id.toString())
 
-            let parsedItem = {
+            const parsedItem = {
                 source: source.sid,
                 title: item.title,
                 link: item.url,
@@ -192,30 +219,35 @@ export const minifluxServiceHooks: ServiceHooks = {
                 content: item.content,
                 snippet: htmlDecode(item.content).trim(),
                 creator: item.author,
-                hasRead: Boolean(item.status === "read"),
-                starred: Boolean(item.starred),
+                hasRead: item.status === "read",
+                starred: item.starred,
                 hidden: false,
                 notify: false,
                 serviceRef: String(item.id),
+                thumb: undefined,
             } as RSSItem
 
             // Try to get the thumbnail of the item
-            let dom = domParser.parseFromString(item.content, "text/html")
-            let baseEl = dom.createElement("base")
-            baseEl.setAttribute(
-                "href",
-                parsedItem.link.split("/").slice(0, 3).join("/")
+            const dom = new DOMParser().parseFromString(
+                item.content,
+                "text/html",
             )
+            const baseEl = dom.createElement("base")
+            baseEl.href = new URL(parsedItem.link).origin
             dom.head.append(baseEl)
-            let img = dom.querySelector("img")
-            if (img && img.src) parsedItem.thumb = img.src
+            const img = dom.querySelector("img")
+            if (img?.src) {
+                parsedItem.thumb = img.src
+            }
 
             if (source.rules) {
                 SourceRule.applyAll(source.rules, parsedItem)
-                if ((item.status === "read") !== parsedItem.hasRead)
+                if ((item.status === "read") !== parsedItem.hasRead) {
                     minifluxServiceHooks.markRead(parsedItem)
-                if (item.starred !== parsedItem.starred)
+                }
+                if (item.starred !== parsedItem.starred) {
                     minifluxServiceHooks.markUnread(parsedItem)
+                }
             }
 
             return parsedItem
@@ -228,27 +260,28 @@ export const minifluxServiceHooks: ServiceHooks = {
     syncItems: () => async (_, getState) => {
         const configs = getState().service as MinifluxConfigs
 
-        const unreadPromise: Promise<Entries> = fetchAPI(
-            configs,
-            "entries?status=unread"
-        ).then(response => response.json())
-        const starredPromise: Promise<Entries> = fetchAPI(
-            configs,
-            "entries?starred=true"
-        ).then(response => response.json())
-        const [unread, starred] = await Promise.all([
-            unreadPromise,
-            starredPromise,
-        ])
+        const queryParams = new URLSearchParams({
+            status: "unread",
+            order: "id",
+            direction: "desc",
+        })
+        const unreadItems: Entry[] = await fetchEntries(configs, queryParams)
+
+        queryParams.delete("status")
+        queryParams.delete("before_entry_id")
+        queryParams.append("starred", "true")
+        const starredItems: Entry[] = await fetchEntries(configs, queryParams)
 
         return [
-            new Set(unread.entries.map((entry: Entry) => String(entry.id))),
-            new Set(starred.entries.map((entry: Entry) => String(entry.id))),
+            new Set(unreadItems.map((entry: Entry) => String(entry.id))),
+            new Set(starredItems.map((entry: Entry) => String(entry.id))),
         ]
     },
 
     markRead: (item: RSSItem) => async (_, getState) => {
-        if (!item.serviceRef) return
+        if (!item.serviceRef) {
+            return
+        }
 
         const body = `{
             "entry_ids": [${item.serviceRef}],
@@ -259,14 +292,18 @@ export const minifluxServiceHooks: ServiceHooks = {
             getState().service as MinifluxConfigs,
             "entries",
             "PUT",
-            body
+            body,
         )
 
-        if (response.status !== 204) throw APIError()
+        if (response.status !== 204) {
+            throw APIError()
+        }
     },
 
     markUnread: (item: RSSItem) => async (_, getState) => {
-        if (!item.serviceRef) return
+        if (!item.serviceRef) {
+            return
+        }
 
         const body = `{
             "entry_ids": [${item.serviceRef}],
@@ -276,7 +313,7 @@ export const minifluxServiceHooks: ServiceHooks = {
             getState().service as MinifluxConfigs,
             "entries",
             "PUT",
-            body
+            body,
         )
     },
 
@@ -313,36 +350,40 @@ export const minifluxServiceHooks: ServiceHooks = {
             }`
             await fetchAPI(configs, "entries", "PUT", body)
         } else {
-            const sources = state.sources
+            const { sources } = state
             await Promise.all(
                 sids.map(sid =>
                     fetchAPI(
                         configs,
                         `feeds/${sources[sid]?.serviceRef}/mark-all-as-read`,
-                        "PUT"
-                    )
-                )
+                        "PUT",
+                    ),
+                ),
             )
         }
     },
 
     star: (item: RSSItem) => async (_, getState) => {
-        if (!item.serviceRef) return
+        if (!item.serviceRef) {
+            return
+        }
 
         await fetchAPI(
             getState().service as MinifluxConfigs,
             `entries/${item.serviceRef}/bookmark`,
-            "PUT"
+            "PUT",
         )
     },
 
     unstar: (item: RSSItem) => async (_, getState) => {
-        if (!item.serviceRef) return
+        if (!item.serviceRef) {
+            return
+        }
 
         await fetchAPI(
             getState().service as MinifluxConfigs,
             `entries/${item.serviceRef}/bookmark`,
-            "PUT"
+            "PUT",
         )
     },
 }
